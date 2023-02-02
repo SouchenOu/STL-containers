9f3bdaf46df13b74ef01baf2093e92d1513caa5d
Soukaina Ouchen <souchen@e1r6p5.1337.ma>
2023-02-02 19:18:18 +0100

    finish std::equal

--- equal.hpp
+++ equal.hpp
@@ -15,7 +15,42 @@
 
 namespace ft{
 
+template <class InputIterator1, class InputIterator2>
 
+    bool equal (InputIterator1 first1, InputIterator1 last1, InputIterator2 first2)
+    {
+        while(first1 != last1)
+        {
+            if(*first1 != *first2)
+            {
+                return 0;
+            }
+            else if(*first1 == *first2)
+            {
+                first1++;
+                first2++;
+            }
+        }
+        return 1;
+    }
+
+    template <class InputIterator1, class InputIterator2, class BinaryPredicate> 
+    bool equal(InputIterator1 first1, InputIterator1 last1, InputIterator2 first2,  BinaryPredicate pre )
+    {
+            while(first1 != last1)
+            {
+                if(!pre(*first1,*first2))
+                {
+                    return 0;
+                }
+                else if(pre(*first1,*first2))
+                {
+                    first1++;
+                    first2++;
+                }
+            }
+            return 1;
+    }
 
 }
 
