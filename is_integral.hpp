6561bfe0f72575bab7902ee70776f5220fdd2121
Soukaina Ouchen <souchen@e1r6p5.1337.ma>
2023-02-02 17:51:17 +0100

    finish is_integral

--- is_integral.hpp
+++ is_integral.hpp
@@ -14,28 +14,29 @@
 #ifndef IS_INTEGRAL_HPP
 # define IS_INTEGRAL_HPP
 
-
 #include <limits>
 
+//Checks whether T is an integral type. Provides the member constant value which is equal to true, if T is the type bool, char, char16_t, char32_t, wchar_t, short, int, long, long long, or any implementation-defined extended integer types, including any signed, unsigned, and cv-qualified variants. Otherwise, value is equal to false.
+/*std::is_integer<T> does not exist.
+That being said, std::numeric_limits<T>::is_integer does exist.*/
+
+// numeric_limits : Provides information about the properties of arithmetic types (either integral or floating-point) in the specific platform for which the library compiles.
+
+// There is difference between is_integer and is_integral (is not the same ishould change it )
 namespace ft
 {
-    template <class T> struct is_integral
+    template <class T> 
+    struct is_integral
     {
             typedef     T   value_type;
 
             //The value of std::numeric_limits<T>::is_integer is true for all integer arithmetic types T and false otherwise. This constant is meaningful for all specializations.
             static const bool val_bool = std::numeric_limits<value_type>::is_integer;
+            //exp std::cout << "Maximum value for int: " << std::numeric_limits<int>::max() << '\n';
+                //std::cout << "int is signed: " << std::numeric_limits<int>::is_signed << '\n';
     };
 
-    template<typename T, typename U>
-        struct is_same 
-    {
-            static const bool val_bool = false; 
-    };
-    template<typename T> struct is_same<T,T>
-    {
-            static const bool val_bool = true; 
-    };
+   
 }
 
 #endif
