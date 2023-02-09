758614c4c26196aa5a4a7a8b96ffe3db29e470fd
Soukaina Ouchen <souchen@e1r7p1.1337.ma>
2023-02-09 15:49:40 +0100

    impliment red_black tree

--- map/map.hpp
+++ map/map.hpp
@@ -21,10 +21,28 @@
 
 namespace ft
 {
+    template<class Key, class T, class Compare = std::less< Key>,class Allocator = std::allocator<std::pair<const Key,T>>>
+    class map
+    {
+        public:
+            typedef T                               mapped_type;
+            typedef Key                             key_type;
+            typedef std::pair<const Key, T>         value_type;
+            typedef Compare                         key_compare;
+            typedef Allocator                       allocator_type;
+        
+        
+        
+        typedef typename allocator_type::reference                  reference;
+        typedef typename allocator_type::const_reference            const_reference;
+        typedef typename  allocator_type::pointer                   pointer;
+        typedef typename  allocator_type::const_pointer             const_pointer;
+        
 
+    };
 			 		 
 	
-};
+}
 
 #endif
 
