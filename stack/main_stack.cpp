69b8b4b685e445a2f0dffb0995fa2fe3a95dff15
Soukaina Ouchen <souchen@e1r7p1.1337.ma>
2023-03-04 20:32:52 +0100

    starting using tester and checking other test for map , vector, stack and set

--- stack/main_stack.cpp
+++ stack/main_stack.cpp
@@ -11,50 +11,50 @@
 /* ************************************************************************** */
 
 
-#include "stack.hpp"
-#include "iostream"
-#include "stack"
+// #include "stack.hpp"
+// #include "iostream"
+// #include "stack"
 
-int main()
-{
-    ft::stack<int> mystack;
-    std::stack<int> mystackTest;
+// int main()
+// {
+//     ft::stack<int> mystack;
+//     std::stack<int> mystackTest;
 
-    int sum(0);
-    int sum2(0);
-    for(int i=1; i <= 10; i++)
-    {
-        mystack.push(i*10);
-        mystackTest.push(i*10);
-    }
-    std::cout << "*********empty()********\n";
-    while(!mystack.empty())
-    {
-        //Since stacks are last-in first-out containers, the top element is the last element inserted into the stack.
-        sum = sum + mystack.top();
-        sum2 = sum2 + mystackTest.top();
-        mystack.pop();
-        mystackTest.pop();
-    }
-    std::cout << "total for our stack :" << sum << "\n";
-    std::cout << "total for stackTest :" << sum2 << "\n";
+//     int sum(0);
+//     int sum2(0);
+//     for(int i=1; i <= 10; i++)
+//     {
+//         mystack.push(i*10);
+//         mystackTest.push(i*10);
+//     }
+//     std::cout << "*********empty()********\n";
+//     while(!mystack.empty())
+//     {
+//         //Since stacks are last-in first-out containers, the top element is the last element inserted into the stack.
+//         sum = sum + mystack.top();
+//         sum2 = sum2 + mystackTest.top();
+//         mystack.pop();
+//         mystackTest.pop();
+//     }
+//     std::cout << "total for our stack :" << sum << "\n";
+//     std::cout << "total for stackTest :" << sum2 << "\n";
 
-    mystack.push(100);
-    mystack.push(200);
-    mystack.push(300);
-    mystack.push(400);
-    mystack.push(500);
-    mystackTest.push(100);
-    mystackTest.push(200);
-    mystackTest.push(300);
-    mystackTest.push(400);
-    mystackTest.push(500);
+//     mystack.push(100);
+//     mystack.push(200);
+//     mystack.push(300);
+//     mystack.push(400);
+//     mystack.push(500);
+//     mystackTest.push(100);
+//     mystackTest.push(200);
+//     mystackTest.push(300);
+//     mystackTest.push(400);
+//     mystackTest.push(500);
 
-    std::cout << "************size()*************\n";
+//     std::cout << "************size()*************\n";
 
-    std::cout << "Size of my stack-->"<< mystack.size() << std::endl;
-    std::cout << "Size of test stack-->" << mystackTest.size() << std::endl;
+//     std::cout << "Size of my stack-->"<< mystack.size() << std::endl;
+//     std::cout << "Size of test stack-->" << mystackTest.size() << std::endl;
 
 
 
-}
+// }
